f6748973b048a33e033c4bcad77bab298750ab27
Raphael Cannatà <raphael.cannata@epfl.ch>
2026-03-25 11:17:28 +0100

    move rooms in dedicate folder

--- scraper/buildings/building_from_rooms.py
+++ scraper/buildings/building_from_rooms.py
@@ -1,6 +1,6 @@
 # extract the buildings from rooms.json, the buildings are the first alphabetical part of the room code, e.g. "BC" from "BC 123"
 import json
-with open("scraper/rooms.json", "r") as f:
+with open("scraper/rooms/rooms.json", "r") as f:
     rooms = json.load(f)
 
 buildings = set()
